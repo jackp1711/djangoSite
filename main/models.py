af57ec29da7837c46c2fc5596cc26280521be5d4
jackp1711 <jackyp98@hotmail.co.uk>
2019-06-05 18:54:40 +0100

    Part 3 completed. Details of the admin page learned and edited, as well as additional apps integrated into the admin pages.

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -6,7 +7,7 @@ from django.db import models
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
-    tutorial_published = models.DateTimeField("date published")
+    tutorial_published = models.DateTimeField("date published", default=datetime.now())
 
     def __str__(self):
         return self.tutorial_title
